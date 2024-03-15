e7e0a615200dd6bd51cd8041ed592a2f8b3a2124
Amorim-EA <erick.amorim@estudante.ifms.edu.br>
2024-03-15 13:50:47 -0400

    feat: add hamburguer menu, and style more responsive to the mobile devices

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,21 +1,37 @@
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Play as IconLogo } from 'lucide-react';
+import { Play, Menu, X as Fechar, GalleryVerticalEnd as Home, Plus as Salvar } from 'lucide-react';
 import './style.scss';
 
 export default function NavBar() {
+    const [ showMenu, setShowMenu ] = useState("-17rem");
+  
     return (
-        <nav className="navbar">
-            <div className="logoUl">
-                <IconLogo />
-                <ul>
-                    <li>
-                        <Link to="/">Videos</Link>
-                    </li>
-                    <li>
-                        <Link to="/cadastrar">Cadastrar</Link>
-                    </li>
-                </ul>
-            </div>
-        </nav>
+      <header>
+          <div>
+              <Play />
+              <Menu className="disapear" onClick={() => setShowMenu("0rem")} />
+              <nav style={{right: showMenu}}>
+                  <ul>
+                      <li className="disapear" onClick={() => setShowMenu("-16rem")}>
+                          <Fechar />
+                          <p>Fechar</p>
+                      </li>
+                      <li className="espacar">
+                          <Link to="/">
+                              <Home className="disapear" />
+                              Videos
+                          </Link>
+                      </li>
+                      <li>
+                          <Link to="/cadastrar">
+                              <Salvar className="disapear" />   
+                              Cadastrar
+                          </Link>
+                      </li>
+                  </ul>
+              </nav>
+          </div>
+      </header>
     );
 }
